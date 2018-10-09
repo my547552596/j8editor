190c3a5f253c597ff815d5d6e67c8f864d8d4284
my547552596 <my611@outlook.com>
2018-10-09 22:05:26 +0800

    修复了 Windows 10 托盘程序恢复窗口的Bug

--- j8editor.h
+++ j8editor.h
@@ -1,19 +1,6 @@
 #include "Atwo.h"
 #include "jet.h"
 
-#define	FILE_READ					0
-#define	FILE_WRITE					1
-#define	TEXT_SEARCH					SW_HIDE
-#define	TEXT_REPLACE				SW_SHOW
-#define	WORD_LOWER					0
-#define	WORD_UPPER					1
-
-#define	IDI_MAINFRAME				128
-#define	IDS_FILE_FILTER				1001
-#define	IDS_FILE_NEW				1002
-#define IDS_ABOUT_TITLE				"关于"
-#define IDS_ABOUT_TEXT				1976
-
 #define MAKE_STRING(N)				#N
 #define MAKE_VERSION(A, B, C, D)	MAKE_STRING(A.B.C.D)
 #define VERSION_INFO				MAKE_VERSION(VI_MAJOR, VI_MINOR, VI_REVISION, VI_BUILD)
@@ -24,6 +11,22 @@
 #define VI_PROGRAM_NAME_CN			"快码加编"
 #define VI_PROGRAM_NAME_EN			"j8editor"
 
+#define	IDI_MAINFRAME				128
+#define	IDS_FILE_FILTER				1001
+#define	IDS_FILE_NEW				1002
+#define IDS_ABOUT_TITLE				"关于"
+#define IDS_ABOUT_TEXT				1976
+
+#define	FILE_READ					0
+#define	FILE_WRITE					1
+#define	TEXT_SEARCH					SW_HIDE
+#define	TEXT_REPLACE				SW_SHOW
+#define	WORD_LOWER					0
+#define	WORD_UPPER					1
+
+#define WM_TASKBAR_TRAY				(WM_USER + 100)
+#define WM_TASKBAR_CREATED			RegisterWindowMessage(TEXT("TaskbarCreated"))
+
 HICON hIcon;
 HWND hDialog, hEditor, hFrame;
 NOTIFYICONDATA nid;
@@ -31,17 +34,21 @@ static const TCHAR cFrameName[9] = VI_PROGRAM_NAME_EN;
 static TCHAR cFilePath[MAX_PATH], cLoadString[MAX_PATH], cSearchString[50], cReplaceString[50];
 
 LRESULT CALLBACK WinProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
-void toClickMenuItem(HWND hWnd, WORD wId);
+void toClickMenuItem(HWND hWnd, WPARAM wParam);
+void toClickNotifyIconData(HWND hWnd, LPARAM lParam);
 BOOL toDrawText(BOOL bCalc, HDC hDC, TCHAR* cBuffer);
 void toHelp(TCHAR *cTitle, TCHAR *cText);
 TCHAR *toLoadString(int iDS);
 void toSelectMenuItem();
 
+void toTest();
+
 /* Layout */
 void toCreateEditor(HWND hParent);
 void toCreateFrame(HINSTANCE hInstance);
 void toCreateNotifyIconData(HWND hWnd);
-void toResizeEdit();
+void toRecoverNotifyIconData(UINT message);
+void toResizeEdit(WPARAM wParam);
 void toSetFrameTitle();
 void toSetNotifyIconData();
 
@@ -50,7 +57,7 @@ BOOL toConfirmFileExist(char *cPath);
 void toCreateFile();
 void toDropFiles(HWND hWnd, HDROP hDropInfo);
 void toGetFileReadonly();
-void toOpenOrSaveAsFile(BOOL iOS);
+void toOpenOrSaveAsFile(BOOL bOS);
 void toPrintFile();
 void toReadFile();
 void toSaveFile();
@@ -61,7 +68,7 @@ void toWriteFile();
 void toClear();
 void toCopyCutPasteSelectallUndo(UINT uMsg);
 void toGetTextAndLineCount();
-void toShiftSel(BOOL bLU);
+void toShiftSel(BOOL bUL);
 
 LRESULT CALLBACK indexProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 void toCreateDialog(HWND hParent);
--- jet.c
+++ jet.c
@@ -1,33 +1,33 @@
 #include "j8editor.h"
 
 char *toGetFileName(char *cPath) {
-	_splitpath(cPath, NULL, NULL, cBuffer64, cBuffer16);
+    _splitpath(cPath, NULL, NULL, cBuffer64, cBuffer16);
 
-	if(strlen(cBuffer16)) {
-		strcat(cBuffer64, ".");
-		strcat(cBuffer64, cBuffer16);
-	}
+    if(strlen(cBuffer16)) {
+        strcat(cBuffer64, ".");
+        strcat(cBuffer64, cBuffer16);
+    }
 
-	return cBuffer64;
+    return cBuffer64;
 }
 
 int toGetIndexOfString(char *str1, char *str2) {
-	char *pos = str1;
-	int index = 0;
+    char *pos = str1;
+    int index = 0;
 
-	if(pos = strstr(str1, str2)) {
-		while(str1 != pos) {
-			str1++;
-			index++;
-		}
-	} else {
-		index = -1;
-	}
+    if(pos = strstr(str1, str2)) {
+        while(str1 != pos) {
+            str1++;
+            index++;
+        }
+    } else {
+        index = -1;
+    }
 
-	return index;
+    return index;
 }
 
 char *toGetStringFromInteger(int iNum) {
-	itoa(iNum, cBuffer16, 10);
-	return cBuffer16;
+    itoa(iNum, cBuffer16, 10);
+    return cBuffer16;
 }

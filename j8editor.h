a54ecf846a771ce87b015e95d1bfe18f8b5589ee
yiyi <my611@outlook.com>
2018-12-17 13:54:03 +0800

    release v1.0 standard

--- j8editor.h
+++ j8editor.h
@@ -1,41 +1,41 @@
 #include "../bo/xSeal_c.h"
 #include "jet.h"
 
-#define MAKE_STRING(N)				#N
+#define MAKE_STRING(N)				    #N
 #define MAKE_VERSION(A, B, C, D)	MAKE_STRING(A.B.C.D)
-#define VERSION_INFO				MAKE_VERSION(VI_MAJOR, VI_MINOR, VI_REVISION, VI_BUILD)
-#define VI_MAJOR					1
-#define VI_MINOR					0
-#define VI_REVISION					1
-#define VI_BUILD					22
+#define VERSION_INFO				      MAKE_VERSION(VI_MAJOR, VI_MINOR, VI_REVISION, VI_BUILD)
+#define VI_MAJOR				        	1
+#define VI_MINOR				        	0
+#define VI_REVISION			      		1
+#define VI_BUILD				        	23
 
-#define	IDI_MAINFRAME				128
-#define IDS_ABOUT					999
-#define IDS_DEFAULT_TITLE			1000
-#define	IDS_FILE_FILTER				1001
-#define	IDS_FILE_NEW				1002
-#define	IDS_INDEX					1101
-#define	IDS_PROGRAM_NAME			1999
+#define	IDI_MAINFRAME		      		128
+#define IDS_ABOUT				         	999
+#define IDS_DEFAULT_TITLE			    1000
+#define	IDS_FILE_FILTER			    	1001
+#define	IDS_FILE_NEW				      1002
+#define	IDS_INDEX					        1101
+#define	IDS_PROGRAM_NAME			    1999
 
-#define	CHAR_ANSI					2001
-#define	CHAR_USC2BE					2116
-#define	CHAR_USC2LE					2016
-#define	CHAR_UTF8					2008
-#define	CHAR_UTF8BOM				2018
+#define	CHAR_ANSI				        	2001
+#define	CHAR_USC2BE				      	2116
+#define	CHAR_USC2LE				      	2016
+#define	CHAR_UTF8				        	2008
+#define	CHAR_UTF8BOM			      	2018
 
-#define	FILE_READ					0
-#define	FILE_WRITE					1
-#define	EDIT_GOTO					-1
-#define	EDIT_REPLACE				0
-#define	EDIT_SEARCH					1
-#define	TRIM_ALL					0
-#define	TRIM_LEFT					1
-#define	TRIM_RIGHT					2
-#define	WORD_LOWER					0
-#define	WORD_UPPER					1
+#define	FILE_READ				        	0
+#define	FILE_WRITE			      		1
+#define	EDIT_GOTO				        	-1
+#define	EDIT_REPLACE			      	0
+#define	EDIT_SEARCH					      1
+#define	TRIM_ALL					        0
+#define	TRIM_LEFT					        1
+#define	TRIM_RIGHT					      2
+#define	WORD_LOWER					      0
+#define	WORD_UPPER					      1
 
-#define WM_TASKBAR_TRAY				(WM_USER + 100)
-#define WM_TASKBAR_CREATED			RegisterWindowMessage(TEXT("TaskbarCreated"))
+#define WM_TASKBAR_TRAY				    (WM_USER + 100)
+#define WM_TASKBAR_CREATED			  RegisterWindowMessage(TEXT("TaskbarCreated"))
 
 HICON hIcon;
 HWND hDialog, hEditor, hFrame;
